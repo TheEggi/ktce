042f345dee09f23dc67c40e37029700067c8aac7
Artur <Artur@test.de>
2011-11-19 19:21:13 +0100

    <Tag 2> Artur: UUID generation

--- trunk/SortedSetExample/src/main/java/at/jku/ce/DomainObject.java
+++ trunk/SortedSetExample/src/main/java/at/jku/ce/DomainObject.java
@@ -1,16 +1,21 @@
 package at.jku.ce;
 
 public class DomainObject {
-    public String id;
+    public String id = UUID.randomUUID().toString()
     private String name;
 
     public DomainObject(String id) {
         super();
         this.id = id;
     }
-
+    /** * Default constructor */
     public DomainObject() {
         super();
+    }
+	
+    public String getId() 
+    { 
+        return id; 
     }
 
 }
