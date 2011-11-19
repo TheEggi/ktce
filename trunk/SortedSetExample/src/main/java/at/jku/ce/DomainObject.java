8e9fa36c4422de1dfed340948f33cef170b74679
Artur <Artur@test.de>
2011-11-19 17:45:19 +0100

    <Tag 2> Clemens: Hinzufügen geter

--- trunk/SortedSetExample/src/main/java/at/jku/ce/DomainObject.java
+++ trunk/SortedSetExample/src/main/java/at/jku/ce/DomainObject.java
@@ -1,9 +1,20 @@
 package at.jku.ce;
 
 public class DomainObject {
+    public String id;
 
-	public DomainObject() {
-		super();
-	}
+    public DomainObject(String id) {
+        super();
+        this.id = id;
+    }
+
+    public DomainObject() {
+        super();
+    }
+	
+    public String getId() 
+    { 
+        return id; 
+    }
 
 }
