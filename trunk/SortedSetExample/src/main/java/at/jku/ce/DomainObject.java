c951cd4e81e969b1cc991ee0fc7429ca79222ce3
Artur <Artur@test.de>
2011-11-19 17:45:19 +0100

    <Tag 1> Artur: Hinzufügen der id und neuer Konstruktor

--- trunk/SortedSetExample/src/main/java/at/jku/ce/DomainObject.java
+++ trunk/SortedSetExample/src/main/java/at/jku/ce/DomainObject.java
@@ -1,9 +1,15 @@
 package at.jku.ce;
 
 public class DomainObject {
+    public String id;
 
-	public DomainObject() {
-		super();
-	}
+    public DomainObject(String id) {
+        super();
+        this.id = id;
+    }
+
+    public DomainObject() {
+        super();
+    }
 
 }
